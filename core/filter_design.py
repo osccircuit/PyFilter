701ff60ed15c4f0d908f81838f4c39518f7ce222
Egor Popov <popoveg18@gmail.com>
2025-03-29 14:41:28 +0300

    Create first version filter sampling

--- core/filter_design.py
+++ core/filter_design.py
@@ -1,9 +1,13 @@
-def freq_samp(n: int, cutoff: list, r: float=1, fs: float=None) -> None:
+import math
+
+def freq_samp(n: int, cutoff: list, r: float=1, fs: float=None) -> dict:
     """Функция расчета фильтра методом частотной выборки"""
+    h_k = 1
     alpha = (n - 1) / 2
-    need_freq_points = []
     m = (n / 2) - 1 if n % 2 == 0 else (n - 1) / 2
     freq_points = [point * fs / n for point in range(n)]
+    need_freq_points = []
+    members = {}
     try:
         if not isinstance(cutoff, list):
             raise TypeError(
@@ -18,11 +22,19 @@ def freq_samp(n: int, cutoff: list, r: float=1, fs: float=None) -> None:
         for index, point in enumerate(freq_points):
             if point >= cutoff[0] and point <= cutoff[1]:
                 need_freq_points.append(index)
-                print(f'{index} -- {point}')
+                members[f'{index}'] = [
+                    2*abs(h_k)*math.cos(2*math.pi*index*alpha/n),
+                    2*r*index*math.cos(2*math.pi/n),
+                    -2*r*abs(h_k)*math.cos(2*math.pi*index*(1 + alpha)/n),
+                    -r**2
+                ]
     except TypeError as e:
         print(f'{e} Выход из программы')
-        raise SystemExit() from e
+        raise SystemExit() from e   
+    return members
 
 
 if __name__ == '__main__':
-    freq_samp(n=10, cutoff=[0, 200], fs=1000)
+    mems = freq_samp(n=41, cutoff=[200, 300], fs=1000)
+    for name, member in mems.items():
+        print(f'{name} - {member}')
